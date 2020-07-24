97f60725249948ce91cd90b4f04cabbaa1ab3cbf
Anirudh <47769671+AnirudhSethuraman@users.noreply.github.com>
2020-07-24 22:20:28 +0530

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,9 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n This is Anirudh');
-  res.send('This is Anirudh\n');
-  res.send('welcome to my Red Hat openshift example \n :) :) ');
+  res.send('Hello World!\n This is Anirudh \n welcome to my Red Hat openshift example \n :) :) ');
 });
 
 app.listen(8080, function () {
