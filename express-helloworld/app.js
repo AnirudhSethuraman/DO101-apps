a80513763b0e6a079e328a410626cbd4a27a47b4
Anirudh <47769671+AnirudhSethuraman@users.noreply.github.com>
2020-07-25 09:24:24 +0530

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n This is Anirudh <br/> welcome to my Red Hat openshift example \n :) :) <img src="https://github.com/AnirudhSethuraman/DO101-apps/blob/master/express-helloworld/__custom_showroom_1557510309.png"  width="80" height="80">');
+  res.send('Hello World!\n This is Anirudh <br/> welcome to my Red Hat openshift example \n :) :) <img src="__custom_showroom_1557510309.png"  width="80" height="80">');
 });
 
 app.listen(8080, function () {
